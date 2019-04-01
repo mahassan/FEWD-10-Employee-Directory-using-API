13f70469962947846fd1dabca4072a52946d5443
Ammar Hassan <ammar@ammarhassan.co.uk>
2019-04-01 12:37:42 +0100

    outputting raw html with results from API

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const app = document.getElementById("app")
+loadAllEvents();
+
+function loadAllEvents(){
+    document.addEventListener("DOMContentLoaded", loadRequest)
+}
+
+function loadRequest(){
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://randomuser.me/api/", true);
+    xhr.onload = function (){
+        const response = JSON.parse(this.responseText);
+        const result = response.results;
+        console.log(result);
+        let output = '';
+        // Image
+        // First and Last name
+        // Email
+        // City
+        for(let person in result){
+            result[person].cell
+           output += `
+            <div class="card">
+                <div class="card-img">
+                    <img src="${result[person].picture.medium}"
+                    alt="${result[person].name.first} ${result[person].name.last}"/>
+                </div>
+                <div class="card-info">
+                    <h5>${result[person].name.first} ${result[person].name.last}</h5>
+                    <p>${result[person].email}</p>
+                    <p>${result[person].location.city}</p>
+                </div>
+            </div>
+           `
+        }
+
+        app.innerHTML = output;
+    }
+    xhr.send();
+}
